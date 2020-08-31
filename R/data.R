d45e2a713d16c201aace72a65eeeef9d497ff715
Devon Buchanan <devon@divinenephron.co.uk>
2020-08-31 18:09:26 +0100

    Added "hemogloin" example data.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Simulated Haemoglobin Results
+#'
+#' @description A simulated hemoglobin result dataset with a true reference
+#'   interval of 12.0-16.0 g/dL.
+#'
+#' @format  A data frame with columns \code{result}, \code{age} and \code{sex}.
+#'   The result column is numeric and the units are g/dL. The age column is
+#'   numeric and the units are years. The sex column is a character vector,
+#'   containing "m" and "w" for men and women.
+#'
+#' @source \url{https://gitlab.miracum.org/kosmic/kosmic}
+"hemoglobin"
