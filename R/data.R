d44c8e599a28592332a79124ef9ae7460afd5c12
Devon Buchanan <devon@divinenephron.co.uk>
2020-09-20 12:54:00 +0100

    Added the "haemoglobin" synonym for the example dataset.

--- R/data.R
+++ R/data.R
@@ -1,7 +1,8 @@
 #' Simulated Haemoglobin Results
 #'
 #' @description A simulated hemoglobin result dataset with a true reference
-#'   interval of 12.0-16.0 g/dL.
+#'   interval of 12.0-16.0 g/dL. Note that "haemoglobin" and "hemoglobin" are
+#'   synonyms.
 #'
 #' @format  A data frame with columns \code{result}, \code{age} and \code{sex}.
 #'   The result column is numeric and the units are g/dL. The age column is
@@ -9,4 +10,7 @@
 #'   containing "m" and "w" for men and women.
 #'
 #' @source \url{https://gitlab.miracum.org/kosmic/kosmic}
+"haemoglobin"
+
+#' @rdname haemoglobin
 "hemoglobin"
---
+++ data/hemoglobin.R
@@ -0,0 +1,9 @@
+# Create a temporary environment to load the
+# haemloglobin data into to avoid overwriting
+# the existing "haemoglobin" variable.
+hemoglobin_env <- new.env()
+load(system.file("data", "haemoglobin.rda",
+                 package = "kosmic"),
+     envir = hemoglobin_env)
+hemoglobin <- hemoglobin_env$haemoglobin
+rm(hemoglobin_env)
